dd2e00be101dfdd77d96a758ac7dd75ac50ffa31
Timofey.Kirillov <timofey.kirillov@flant.ru>
2016-02-18 15:40:50 +0300

    Add to cgroup.

--- pam_docker.c
+++ pam_docker.c
@@ -7,8 +7,13 @@
 #include <syslog.h>
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <sys/stat.h>
+#include <linux/limits.h>
 #include <security/pam_modules.h>
 #include <security/pam_ext.h>
 
@@ -20,7 +25,10 @@
 #define PAM_DOCKER_SECURITY_CONF "/etc/security/docker.conf"
 #endif
 
-#define PAM_DOCKER_STRINGIZE(x) #x
+#ifndef PAM_DOCKER_CGROUP_ROOT_PATH
+#define PAM_DOCKER_CGROUP_ROOT_PATH "/sys/fs/cgroup"
+#endif
+
 #define PAM_DOCKER_NAME_MAX_SIZE 256
 #define PAM_DOCKER_ID_SIZE 64
 
@@ -89,7 +97,7 @@ static int pid_and_id_of_docker_container(pam_handle_t* pamh, const char* docker
         if(strstr(response, http_404_search) == response) {
             pam_syslog(pamh, LOG_ERR, "docker container '%s' not found", docker_container);
             goto response_error;
-        } else if (strstr(response, http_ok_search) != response) {
+        } else if(strstr(response, http_ok_search) != response) {
             pam_syslog(pamh, LOG_ERR, "non 200 docker api http answer");
             goto response_error;
         }
@@ -123,6 +131,52 @@ error:
     return 1;
 }
 
+static int add_to_cgroup(pam_handle_t* pamh, pid_t pid, const char* cgroup) {
+    static const char* cgroup_root_path = PAM_DOCKER_CGROUP_ROOT_PATH;
+    DIR* dir;
+    struct dirent entry;
+    struct dirent* result;
+    char pid_str[32];
+    char path[PATH_MAX];
+    int fd;
+
+    dir = opendir(cgroup_root_path);
+    if(dir == NULL) {
+        pam_syslog(pamh, LOG_ERR, "failed to open cgroup fs at %s", cgroup_root_path);
+        goto error;
+    }
+
+    snprintf(pid_str, sizeof(pid_str), "%d", pid);
+
+    while(readdir_r(dir, &entry, &result) == 0 && result != NULL) {
+        if(entry.d_type != DT_DIR) continue;
+        if(strcmp(entry.d_name, ".") == 0 || strcmp(entry.d_name, "..") == 0) continue;
+
+        snprintf(path, sizeof(path), "%s/%s/docker/%s/tasks", cgroup_root_path, entry.d_name, cgroup);
+
+        if(access(path, F_OK) == 0) {
+            fd = open(path, O_WRONLY | O_APPEND);
+            if(fd < 0) {
+                pam_syslog(pamh, LOG_ERR, "failed to open tasks of cgroup '%s'", entry.d_name);
+                goto error;
+            } else if(write(fd, pid_str, strlen(pid_str)) != (long long)strlen(pid_str)) {
+                pam_syslog(pamh, LOG_ERR, "failed to add task '%s' to cgroup '%s'", pid_str, entry.d_name);
+                goto cgroup_write_error;
+            }
+
+            close(fd);
+        }
+    }
+
+    closedir(dir);
+    return 0;
+
+cgroup_write_error:
+    close(fd);
+error:
+    return 1;
+}
+
 PAM_EXTERN int pam_sm_open_session(pam_handle_t* pamh, int flags, int argc, const char** argv) {
     (void)pamh;
     (void)flags;
@@ -138,19 +192,25 @@ PAM_EXTERN int pam_sm_open_session(pam_handle_t* pamh, int flags, int argc, cons
     rc = pam_get_user(pamh, (const char **)(&username), NULL);
     if(rc != PAM_SUCCESS) {
         pam_syslog(pamh, LOG_ERR, "cannot fetch user name");
-        return PAM_SESSION_ERR;
+        goto error;
     }
 
     rc = docker_container_of_user(pamh, username, docker_container, sizeof(docker_container));
     if(rc != 0) {
         pam_syslog(pamh, LOG_ERR, "cannot determine docker container of user '%s'", username);
-        return PAM_SESSION_ERR;
+        goto error;
     }
 
     rc = pid_and_id_of_docker_container(pamh, docker_container, &docker_pid, docker_id);
     if(rc != 0) {
         pam_syslog(pamh, LOG_ERR, "cannot determine pid and id of docker container '%s'", docker_container);
-        return PAM_SESSION_ERR;
+        goto error;
+    }
+
+    rc = add_to_cgroup(pamh, getpid(), docker_id);
+    if(rc != 0) {
+        pam_syslog(pamh, LOG_ERR, "cannot add session to cgroup '%s'", docker_id);
+        goto error;
     }
 
     pam_syslog(
@@ -159,6 +219,9 @@ PAM_EXTERN int pam_sm_open_session(pam_handle_t* pamh, int flags, int argc, cons
     );
 
     return PAM_SUCCESS;
+
+error:
+    return PAM_SESSION_ERR;
 }
 
 PAM_EXTERN int pam_sm_close_session(pam_handle_t* pamh, int flags, int argc, const char** argv) {
