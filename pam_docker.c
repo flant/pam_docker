87fe4d1c3694fd4028895d2e18b4f88e542d4c49
Timofey.Kirillov <timofey.kirillov@flant.ru>
2016-02-19 15:41:18 +0300

    Parse /etc/security/docker.conf to find docker container.

--- pam_docker.c
+++ pam_docker.c
@@ -11,6 +11,9 @@
 #include <dirent.h>
 #include <fcntl.h>
 #include <sched.h>
+#include <pwd.h>
+#include <grp.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
@@ -23,8 +26,8 @@
 #define PAM_DOCKER_SOCK "/var/run/docker.sock"
 #endif
 
-#ifndef PAM_DOCKER_SECURITY_CONF
-#define PAM_DOCKER_SECURITY_CONF "/etc/security/docker.conf"
+#ifndef PAM_DOCKER_SECURITY_CONF_PATH
+#define PAM_DOCKER_SECURITY_CONF_PATH "/etc/security/docker.conf"
 #endif
 
 #ifndef PAM_DOCKER_CGROUP_ROOT_PATH
@@ -34,24 +37,141 @@
 #define PAM_DOCKER_NAME_MAX_SIZE 256
 #define PAM_DOCKER_ID_SIZE 64
 
-static int docker_container_of_user(pam_handle_t* pamh, const char* username,
-                                    char* docker_container, size_t docker_container_size) {
-    (void)pamh;
-    (void)username;
+static int docker_container_of_user(pam_handle_t *pamh, const char *username,
+                                    char *result_container, size_t result_container_size) {
+    static const char *security_conf_path = PAM_DOCKER_SECURITY_CONF_PATH;
+    int rc;
+    struct passwd *passwd_entry;
+    uid_t uid;
+    gid_t gids[NGROUPS_MAX];
+    int ngroups = sizeof(gids)/sizeof(*gids);
+    FILE *file;
+    char *line = NULL;
+    size_t line_len = 0;
+    size_t line_num;
+    int ret = 1;
 
-    snprintf(docker_container, docker_container_size, "mycontainer");
+    passwd_entry = getpwnam(username);
+    if(passwd_entry == NULL) {
+        pam_syslog(pamh, LOG_ERR, "cannot find passwd entry for user '%s'", username);
+        goto error;
+    }
 
-    return 0;
+    uid = passwd_entry->pw_uid;
+
+    rc = getgrouplist(username, passwd_entry->pw_gid, gids, &ngroups);
+    if(rc == -1) {
+        pam_syslog(pamh, LOG_ERR, "cannot fetch groups of user '%s'", username);
+        goto error;
+    }
+
+    file = fopen(security_conf_path, "r");
+    if(file == NULL) {
+        pam_syslog(pamh, LOG_ERR, "failed to open docker security config %s", security_conf_path);
+        goto error;
+    }
+
+    for(line_num = 1; ; line_num++) {
+        char *line_p;
+        char *saveptr;
+        char *check_domain, *container, *token;
+        static const char *delim = " \t";
+        int i;
+
+        rc = getline(&line, &line_len, file);
+        if(rc == -1) break;
+
+        line_p = strchr(line, '#');
+        if(line_p) *line_p = '\0';
+
+        line_p = strchr(line, '\n');
+        if(line_p) *line_p = '\0';
+
+        line_p = line;
+        while(*line_p && isspace(*line_p)) line_p++;
+        if(strlen(line_p) == 0) continue;
+
+        check_domain = strtok_r(line_p, delim, &saveptr);
+        if(check_domain == NULL) {
+            pam_syslog(pamh, LOG_ERR, "docker security config error: %s:%zu: no domain specified",
+                security_conf_path, line_num);
+            goto security_conf_error;
+        }
+
+        container = strtok_r(NULL, delim, &saveptr);
+        if(container == NULL) {
+            pam_syslog(pamh, LOG_ERR, "docker security config error: %s:%zu: no container specified",
+                security_conf_path, line_num);
+            goto security_conf_error;
+        } else if(strlen(container) >= result_container_size) {
+            pam_syslog(pamh, LOG_ERR, "docker security config error: %s:%zu: too long container name '%s'",
+                security_conf_path, line_num, container);
+            goto security_conf_error;
+        }
+
+        token = strtok_r(NULL, delim, &saveptr);
+        if(token != NULL) {
+            pam_syslog(pamh, LOG_ERR, "docker security config error: %s:%zu: unexpected token '%s'",
+                security_conf_path, line_num, token);
+            goto security_conf_error;
+        }
+
+        if(check_domain[0] == '@') {
+            char *check_group = check_domain + 1;
+            struct group *check_group_entry;
+
+            check_group_entry = getgrnam(check_group);
+            if(!check_group_entry) {
+                pam_syslog(pamh, LOG_ERR, "docker security config error: %s:%zu: unknown group '%s'",
+                    security_conf_path, line_num, check_group);
+                goto security_conf_error;
+            }
+
+            for(i = 0; i < ngroups; i++) {
+                if(check_group_entry->gr_gid == gids[i]) {
+                    strncpy(result_container, container, result_container_size);
+                    ret = 0;
+                    goto out;
+                }
+            }
+        } else {
+            char *check_username = check_domain;
+            struct passwd *check_passwd_entry;
+
+            check_passwd_entry = getpwnam(check_username);
+            if(!check_passwd_entry) {
+                pam_syslog(pamh, LOG_ERR, "docker security config error: %s:%zu: unknown user '%s'",
+                    security_conf_path, line_num, check_username);
+                goto security_conf_error;
+            }
+
+            if(check_passwd_entry->pw_uid == uid) {
+                strncpy(result_container, container, result_container_size);
+                ret = 0;
+                goto out;
+            }
+        }
+    }
+
+out:
+    free(line);
+    fclose(file);
+    return ret;
+security_conf_error:
+    free(line);
+    fclose(file);
+error:
+    return 1;
 }
 
-static int pid_and_id_of_docker_container(pam_handle_t* pamh, const char* docker_container,
-                                          pid_t* docker_pid_p, char* docker_id) {
+static int pid_and_id_of_docker_container(pam_handle_t *pamh, const char *docker_container,
+                                          pid_t *docker_pid_p, char *docker_id) {
     int rc;
     int sock;
     struct sockaddr_un server;
-    static const char* docker_sock = PAM_DOCKER_SOCK;
+    static const char *docker_sock = PAM_DOCKER_SOCK;
     char message[256];
-    char* response = NULL;
+    char *response = NULL;
 
     sock = socket(AF_UNIX, SOCK_STREAM, 0);
     if(sock < 0) {
@@ -79,13 +199,13 @@ static int pid_and_id_of_docker_container(pam_handle_t* pamh, const char* docker
     {
         size_t response_read_size = 0;
         int n;
-        char* match;
+        char *match;
         char fmt[32];
         static const size_t response_read_chunk = 4096;
-        static const char* http_404_search = "HTTP/1.0 404";
-        static const char* http_ok_search = "HTTP/1.0 200";
-        static const char* pid_search = "\"Pid\":";
-        static const char* id_search = "\"Id\":\"";
+        static const char *http_404_search = "HTTP/1.0 404";
+        static const char *http_ok_search = "HTTP/1.0 200";
+        static const char *pid_search = "\"Pid\":";
+        static const char *id_search = "\"Id\":\"";
 
         do {
             response = realloc(response, response_read_size + response_read_chunk);
@@ -126,7 +246,6 @@ static int pid_and_id_of_docker_container(pam_handle_t* pamh, const char* docker
     free(response);
     close(sock);
     return 0;
-
 response_error:
     free(response);
 request_error:
@@ -135,14 +254,14 @@ error:
     return 1;
 }
 
-static int add_to_cgroup(pam_handle_t* pamh, pid_t pid, const char* cgroup) {
-    static const char* cgroup_root_path = PAM_DOCKER_CGROUP_ROOT_PATH;
-    DIR* dir;
+static int add_to_cgroup(pam_handle_t *pamh, pid_t pid, const char *cgroup) {
+    static const char *cgroup_root_path = PAM_DOCKER_CGROUP_ROOT_PATH;
+    DIR *dir;
     struct dirent entry;
-    struct dirent* result;
+    struct dirent *result;
     char pid_str[32];
     char path[PATH_MAX];
-    int fd;
+    int fd = -1;
 
     dir = opendir(cgroup_root_path);
     if(dir == NULL) {
@@ -175,17 +294,16 @@ static int add_to_cgroup(pam_handle_t* pamh, pid_t pid, const char* cgroup) {
 
     closedir(dir);
     return 0;
-
 cgroup_write_error:
     close(fd);
 error:
     return 1;
 }
 
-static int add_to_namespaces(pam_handle_t* pamh, pid_t namespace_target_pid) {
+static int add_to_namespaces(pam_handle_t *pamh, pid_t namespace_target_pid) {
     int ret = 0;
     char pathbuf[PATH_MAX];
-    char* ns[] = {"ipc", "uts", "pid", "mnt"};
+    char *ns[] = {"ipc", "uts", "pid", "mnt"};
     size_t n = sizeof(ns)/sizeof(*ns);
     int ns_fd[n];
     size_t i;
@@ -214,21 +332,20 @@ out:
             close(ns_fd[i]);
 
     return ret;
-
 error:
     ret = 1;
     goto out;
 }
 
 
-PAM_EXTERN int pam_sm_open_session(pam_handle_t* pamh, int flags, int argc, const char** argv) {
+PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     (void)pamh;
     (void)flags;
     (void)argc;
     (void)argv;
 
-    char * username;
-    char docker_container[PAM_DOCKER_NAME_MAX_SIZE];
+    char *username;
+    char docker_container[PAM_DOCKER_NAME_MAX_SIZE + 1];
     char docker_id[PAM_DOCKER_ID_SIZE + 1];
     pid_t docker_pid;
     int rc;
@@ -270,18 +387,17 @@ PAM_EXTERN int pam_sm_open_session(pam_handle_t* pamh, int flags, int argc, cons
     );
 
     return PAM_SUCCESS;
-
 error:
     return PAM_SESSION_ERR;
 }
 
-PAM_EXTERN int pam_sm_close_session(pam_handle_t* pamh, int flags, int argc, const char** argv) {
+PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     (void)pamh;
     (void)flags;
     (void)argc;
     (void)argv;
 
-    char * username;
+    char *username;
     pam_get_user(pamh, (const char **)(&username), NULL);
     pam_syslog(pamh, LOG_DEBUG, "close session for user '%s'", username);
 
